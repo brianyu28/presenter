5d9c1b975d5f26db675ef65e5bedb7af1d1b7081
Brian Yu <brian@brianyu.me>
2026-06-27 09:04:14 -1000

    Fix simultaneous editing of multiple presentation variables

--- src/renderer/browser-canvas/utils/variables/mountVariableControls.ts
+++ src/renderer/browser-canvas/utils/variables/mountVariableControls.ts
@@ -1,4 +1,8 @@
-import { setVariableValue } from "../../../../types/Variable";
+import {
+  getRegisteredVariables,
+  getVariableValue,
+  setVariableValue,
+} from "../../../../types/Variable";
 import { storePendingVariableValues } from "../../../../utils/storage/variableStorage";
 import { createVariableControlsElement } from "./createVariableControlsElement";
 
@@ -11,7 +15,7 @@ export function mountVariableControls({ container, onCommit }: Args): void {
   const variablesContainer = createVariableControlsElement({
     onPreview: (id, value) => {
       const nextValue = setVariableValue(id, value);
-      storePendingVariableValues({ [id]: nextValue });
+      storePendingVariableValues(getCurrentVariableValues());
       return nextValue;
     },
     onCommit,
@@ -20,4 +24,14 @@ export function mountVariableControls({ container, onCommit }: Args): void {
   if (variablesContainer !== null) {
     container.appendChild(variablesContainer);
   }
+}
+
+function getCurrentVariableValues(): Record<string, number> {
+  const values: Record<string, number> = {};
+
+  for (const variable of getRegisteredVariables()) {
+    values[variable.id] = getVariableValue(variable.id);
+  }
+
+  return values;
 }
